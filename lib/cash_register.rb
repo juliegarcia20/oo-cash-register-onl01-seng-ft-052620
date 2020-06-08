106852e0b191c72083c73a7abc687c0d22356d0a
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-08 17:57:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(discount = 0)
-    @total = 0.99
+    @total = 0
     @discount = discount
     @items = []
   end
